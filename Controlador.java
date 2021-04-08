5b1f9578331444f4c1b04abcc6bf81911cb085a5
Jorge Rodríguez <jorge@grupostudium.com>
2021-04-08 11:02:01 +0200

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,6 +1,91 @@
 package es.studium.Monomios;
 
-public class Controlador
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+public class Controlador implements WindowListener, ActionListener
+{
+	Monomio monomio;
+	Vista vista;
+	
+	Controlador(Vista v, Monomio m)
+	{
+		this.vista = v;
+		this.monomio = m;
+		// Añadir Listeners
+		this.vista.ventana.addWindowListener(this);
+		this.vista.unMonomio.addWindowListener(this);
+		this.vista.dosMonomios.addWindowListener(this);
+		
+		this.vista.btnAceptarUno.addActionListener(this);
+		this.vista.btnAceptarDos.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0)
+	{
+		if(this.vista.unMonomio.isActive())
+		{
+			this.vista.unMonomio.setVisible(false);
+		}
+		else if (this.vista.dosMonomios.isActive())
+		{
+			this.vista.dosMonomios.setVisible(false);
+		}
+		else
+		{
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
